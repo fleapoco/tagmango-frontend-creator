ec44cb02bca8de3dc15db1605b1337642af47e2d
Prasun Roy <prasun.roy@fleapo.com>
2024-01-03 16:41:35 +0530

    Integrated create degree API

--- components/form/input.tsx
+++ components/form/input.tsx
@@ -1,32 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import type { DatePickerProps } from 'antd';
-import { DatePicker, Input, TimePicker } from 'antd';
-import dayjs, { Dayjs } from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import type { DatePickerProps } from "antd";
+import { DatePicker, Input, TimePicker } from "antd";
+import dayjs, { Dayjs } from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 dayjs.extend(customParseFormat);
 
-const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+const onChange: DatePickerProps["onChange"] = (date, dateString) => {
   console.log(date, dateString);
 };
 
 interface Props {
   label?: string;
+  name?: string;
   type?:
-    | 'search'
-    | 'text'
-    | 'password'
-    | 'email'
-    | 'date'
-    | 'time'
-    | 'month'
-    | 'week'
-    | 'link'
-    | 'number';
+    | "search"
+    | "text"
+    | "password"
+    | "email"
+    | "date"
+    | "time"
+    | "month"
+    | "week"
+    | "link"
+    | "number";
 
   placeholder?: string;
-  variant?: 'dark';
+  variant?: "dark";
   value?: string | number;
   icon?: React.ReactNode;
   required?: boolean;
@@ -38,52 +39,52 @@ interface Props {
 }
 
 export const FormInput = (props: Props) => {
-  let _className = '';
+  let _className = "";
 
-  if (props.variant === 'dark') _className += 'dark-variant-input ';
-  else _className += '';
+  if (props.variant === "dark") _className += "dark-variant-input ";
+  else _className += "";
 
   return (
     <>
-      <div className='form-group'>
-        {props.label ? <label htmlFor='label'>{props.label}</label> : ''}
+      <div className="form-group">
+        {props.label ? <label htmlFor="label">{props.label}</label> : ""}
 
-        {props.type === 'date' ? (
+        {props.type === "date" ? (
           <DatePicker
             allowClear={false}
             disabledDate={(currentDate: dayjs.ConfigType) =>
-              currentDate! < dayjs().startOf('day')
+              currentDate! < dayjs().startOf("day")
             }
             value={dayjs(props.value)}
             onChange={(date, dateString) => {
               props.onDateChange?.(date, dateString);
             }}
-            style={{ width: '100%', padding: '7.5px 12px' }}
+            style={{ width: "100%", padding: "7.5px 12px" }}
           />
-        ) : props.type === 'time' ? (
+        ) : props.type === "time" ? (
           <TimePicker
             defaultValue={dayjs(props.value)}
-            format='h:mm A'
+            format="h:mm A"
             allowClear={false}
-            style={{ width: '100%', padding: '7.5px 12px' }}
+            style={{ width: "100%", padding: "7.5px 12px" }}
             onChange={(time, timeString) => {
               props.onTimeChange?.(time, timeString);
             }}
           />
-        ) : props.type === 'month' ? (
+        ) : props.type === "month" ? (
           <DatePicker
             allowClear={false}
-            picker='month'
+            picker="month"
             value={dayjs(props.value)}
-            style={{ width: '100%', padding: '7.5px 12px' }}
+            style={{ width: "100%", padding: "7.5px 12px" }}
             onChange={(date, dateString) => {
               props.onDateChange?.(date, dateString);
             }}
           />
-        ) : props.type === 'week' ? (
+        ) : props.type === "week" ? (
           <DatePicker
-            picker='week'
-            style={{ width: '100%', padding: '7.5px 12px' }}
+            picker="week"
+            style={{ width: "100%", padding: "7.5px 12px" }}
           />
         ) : (
           <Input
@@ -94,9 +95,10 @@ export const FormInput = (props: Props) => {
             type={props.type}
             disabled={props.disabled}
             className={_className}
-            id='label'
+            id="label"
             value={props.value}
-            style={{ padding: '7.5px 12px' }}
+            style={{ padding: "7.5px 12px" }}
+            name={props.name}
           />
         )}
       </div>
--- src/app/creator/degree/newcertification/page.tsx
+++ src/app/creator/degree/newcertification/page.tsx
@@ -1,57 +1,116 @@
-'use client';
+"use client";
 
-import { Col, Flex, Row } from 'antd';
-import { useRouter } from 'next/navigation';
-import { BreadCrumbNav } from '../../../../../components/common/breadcrumb';
-import { PrimaryButton } from '../../../../../components/common/button';
-import ImageUpload from '../../../../../components/form/imgupload';
-import { FormInput } from '../../../../../components/form/input';
-import { FormTextArea } from '../../../../../components/form/textarea';
-import PageTitle from '../../../../../components/pagetitle';
-import style from '../../../../../style/creator.module.scss';
+import useAPI from "@/hooks/useApi";
+import { Col, Flex, Row } from "antd";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { BreadCrumbNav } from "../../../../../components/common/breadcrumb";
+import { PrimaryButton } from "../../../../../components/common/button";
+import ImageUpload from "../../../../../components/form/imgupload";
+import { FormInput } from "../../../../../components/form/input";
+import { FormTextArea } from "../../../../../components/form/textarea";
+import PageTitle from "../../../../../components/pagetitle";
+import style from "../../../../../style/creator.module.scss";
 
 const breadCrumbItems = [
   {
-    title: 'Back to Degree',
-    link: '/creator/degree',
+    title: "Back to Degree",
+    link: "/creator/degree",
   },
 ];
 
+type DegreeDataType = {
+  title: string;
+  degreeLink: string;
+  description: string;
+  thumbnailUrl: string;
+};
+
 const NewCertification = () => {
   const router = useRouter();
+  const { createDegree } = useAPI();
+
+  const [degreeDataType, setDegreeDataType] = useState<DegreeDataType>({
+    title: "",
+    degreeLink: "",
+    description: "",
+    thumbnailUrl:
+      "https://images.unsplash.com/photo-1610878180933-123728745d22?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  });
+
+  const handleOnChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    if (e.target.name === "title")
+      setDegreeDataType((prev) => {
+        return { ...prev, title: e.target.value };
+      });
+    if (e.target.name === "degreeLink")
+      setDegreeDataType((prev) => {
+        return { ...prev, degreeLink: e.target.value };
+      });
+    if (e.target.name === "description")
+      setDegreeDataType((prev) => {
+        return { ...prev, description: e.target.value };
+      });
+  };
+
+  const handleSubmit = async () => {
+    let data = await createDegree(degreeDataType);
+
+    if (data?.id) {
+      router.push("/creator/degree");
+    }
+  };
+
   return (
     <>
       <div
-        className={`${style['creator-task-details-form']} common-panel-wrapper`}
+        className={`${style["creator-task-details-form"]} common-panel-wrapper`}
       >
         {/* Page Title */}
-        <Row className='p-15'>
+        <Row className="p-15">
           <Col span={24}>
             <BreadCrumbNav item={breadCrumbItems} />
-            <PageTitle title='New Degree' />
+            <PageTitle title="New Degree" />
           </Col>
         </Row>
-        <Row gutter={[12, 0]} className='p-r-b-l-15 '>
+        <Row gutter={[12, 0]} className="p-r-b-l-15 ">
           <Col span={12}>
-            <div className='border-box p-15'>
+            <div className="border-box p-15">
               <ImageUpload />
-              <FormInput label='Degree Title' placeholder='Add Title' />
               <FormInput
-                label='Add Link'
-                type='link'
-                placeholder='Add Degree Link'
+                label="Certification Title"
+                onChange={handleOnChange}
+                placeholder="Add Title"
+                name="title"
               />
-              <div className='form-group'>
-                <label htmlFor='requirement'>Requirement</label>
-                <FormTextArea placeholder='Description' />
+              <FormInput
+                label="Add Link"
+                type="link"
+                onChange={handleOnChange}
+                placeholder="Add Certification Link"
+                name="degreeLink"
+              />
+              <div className="form-group">
+                <label htmlFor="requirement">Requirement</label>
+                <FormTextArea
+                  placeholder="Description"
+                  onChange={handleOnChange}
+                  name="description"
+                />
               </div>
-              <Flex gap={'middle'} justify='end'>
+              <Flex gap={"middle"} justify="end">
                 <PrimaryButton
-                  text='Cancel'
-                  variant='secondary'
+                  text="Cancel"
+                  variant="secondary"
                   onClick={() => router.back()}
                 />
-                <PrimaryButton text='Save' variant='primary' />
+                <PrimaryButton
+                  text="Save"
+                  variant="primary"
+                  onClick={handleSubmit}
+                />
               </Flex>
             </div>
           </Col>
