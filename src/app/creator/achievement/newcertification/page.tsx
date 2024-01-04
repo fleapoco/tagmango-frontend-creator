5c2eef747d8dead8f011f1ed941340a4180c6142
Chetan Mane <119874815+chetan-fleapo@users.noreply.github.com>
2024-01-04 13:03:16 +0530

    Fixed UI iissues

--- src/app/creator/achievement/newcertification/page.tsx
+++ src/app/creator/achievement/newcertification/page.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import useAPI from "@/hooks/useApi";
-import { APIError, UserAchievement } from "@/types";
-import { Col, Flex, Row } from "antd";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useState } from "react";
-import { BreadCrumbNav } from "../../../../../components/common/breadcrumb";
-import { PrimaryButton } from "../../../../../components/common/button";
-import ImageUpload from "../../../../../components/form/imgupload";
-import { FormInput } from "../../../../../components/form/input";
-import { FormTextArea } from "../../../../../components/form/textarea";
-import PageTitle from "../../../../../components/pagetitle";
-import style from "../../../../../style/creator.module.scss";
+import useAPI from '@/hooks/useApi';
+import { APIError, UserAchievement } from '@/types';
+import { Col, Flex, Row } from 'antd';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, useState } from 'react';
+import { BreadCrumbNav } from '../../../../../components/common/breadcrumb';
+import { PrimaryButton } from '../../../../../components/common/button';
+import ImageUpload from '../../../../../components/form/imgupload';
+import { FormInput } from '../../../../../components/form/input';
+import { FormTextArea } from '../../../../../components/form/textarea';
+import PageTitle from '../../../../../components/pagetitle';
+import style from '../../../../../style/creator.module.scss';
 
 const breadCrumbItems = [
   {
-    title: "Back to Achievement",
-    link: "/creator/achievement",
+    title: 'Back to Achievement',
+    link: '/creator/achievement',
   },
 ];
 
@@ -32,9 +32,9 @@ const NewCertification = () => {
 
   const [achievementDataType, setAchievementDataType] =
     useState<AchievementDataType>({
-      title: "",
-      description: "",
-      thumbnailUrl: "",
+      title: '',
+      description: '',
+      thumbnailUrl: '',
     });
 
   const isValidFormData = (achievementData: AchievementDataType) => {
@@ -56,70 +56,70 @@ const NewCertification = () => {
   const handleOnChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    if (e.target.name === "title")
+    if (e.target.name === 'title')
       setAchievementDataType((prev) => {
         return { ...prev, title: e.target.value };
       });
-    else if (e.target.name === "description")
+    else if (e.target.name === 'description')
       setAchievementDataType((prev) => {
         return { ...prev, description: e.target.value };
       });
   };
 
   const handleSubmit = async () => {
-    if (!isValidFormData) alert("Invalid/Missing Input Data");
+    if (!isValidFormData) alert('Invalid/Missing Input Data');
 
     try {
       let data: APIError | UserAchievement = await createAchievement(
         achievementDataType
       );
 
-      if ("error" in data) {
+      if ('error' in data) {
         alert(data?.message);
         return;
       }
 
-      router.push("/creator/achievement");
+      router.push('/creator/achievement');
     } catch (error) {}
   };
 
   return (
     <>
-      <div className={`${style["creator-task-details-form"]} `}>
+      <div className={`${style['creator-task-details-form']} `}>
         {/* Page Title */}
-        <Row className="p-15">
+        <Row className='p-15'>
           <Col span={24}>
             <BreadCrumbNav item={breadCrumbItems} />
-            <PageTitle title="New Achievement" />
+            <PageTitle title='New Achievement' />
           </Col>
         </Row>
-        <Row gutter={[12, 0]} className="p-r-b-l-15 ">
+        <Row gutter={[12, 0]}>
           <Col span={12}>
-            <div className="border-box p-15">
+            <div className='border-box p-15'>
               <ImageUpload handleUpload={handleUpload} />
               <FormInput
-                label="Certification Title"
-                placeholder="Add Title"
+                label='Certification Title'
+                placeholder='Add Title'
                 onChange={handleOnChange}
-                name="title"
+                name='title'
               />
-              <div className="form-group">
-                <label htmlFor="requirement">Requirement</label>
+              <div className='form-group'>
+                <label htmlFor='requirement'>Requirement</label>
                 <FormTextArea
-                  placeholder="Description"
+                  placeholder='Description'
                   onChange={handleOnChange}
-                  name="description"
+                  name='description'
                 />
               </div>
-              <Flex gap={"middle"} justify="end">
+              <Flex gap={'middle'} justify='end'>
                 <PrimaryButton
-                  text="Cancel"
-                  variant="secondary"
+                  text='Cancel'
+                  variant='secondary'
                   onClick={() => router.back()}
                 />
                 <PrimaryButton
-                  text="Save"
-                  variant="primary"
+                  text='Save'
+                  variant='primary'
                   disabled={!isValidFormData(achievementDataType)}
                   onClick={handleSubmit}
                 />
--- src/app/creator/degree/newcertification/page.tsx
+++ src/app/creator/degree/newcertification/page.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import useAPI from "@/hooks/useApi";
-import { APIError, UserDegree } from "@/types";
-import { Col, Flex, Row } from "antd";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useState } from "react";
-import { BreadCrumbNav } from "../../../../../components/common/breadcrumb";
-import { PrimaryButton } from "../../../../../components/common/button";
-import ImageUpload from "../../../../../components/form/imgupload";
-import { FormInput } from "../../../../../components/form/input";
-import { FormTextArea } from "../../../../../components/form/textarea";
-import PageTitle from "../../../../../components/pagetitle";
-import style from "../../../../../style/creator.module.scss";
+import useAPI from '@/hooks/useApi';
+import { APIError, UserDegree } from '@/types';
+import { Col, Flex, Row } from 'antd';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, useState } from 'react';
+import { BreadCrumbNav } from '../../../../../components/common/breadcrumb';
+import { PrimaryButton } from '../../../../../components/common/button';
+import ImageUpload from '../../../../../components/form/imgupload';
+import { FormInput } from '../../../../../components/form/input';
+import { FormTextArea } from '../../../../../components/form/textarea';
+import PageTitle from '../../../../../components/pagetitle';
+import style from '../../../../../style/creator.module.scss';
 
 const breadCrumbItems = [
   {
-    title: "Back to Degree",
-    link: "/creator/degree",
+    title: 'Back to Degree',
+    link: '/creator/degree',
   },
 ];
 
@@ -32,10 +32,10 @@ const NewCertification = () => {
   const { createDegree } = useAPI();
 
   const [degreeDataType, setDegreeDataType] = useState<DegreeDataType>({
-    title: "",
-    degreeLink: "",
-    description: "",
-    thumbnailUrl: "",
+    title: '',
+    degreeLink: '',
+    description: '',
+    thumbnailUrl: '',
   });
 
   const isValidFormData = (degreeData: DegreeDataType) => {
@@ -58,82 +58,80 @@ const NewCertification = () => {
   const handleOnChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    if (e.target.name === "title")
+    if (e.target.name === 'title')
       setDegreeDataType((prev) => {
         return { ...prev, title: e.target.value };
       });
-    if (e.target.name === "degreeLink")
+    if (e.target.name === 'degreeLink')
       setDegreeDataType((prev) => {
         return { ...prev, degreeLink: e.target.value };
       });
-    if (e.target.name === "description")
+    if (e.target.name === 'description')
       setDegreeDataType((prev) => {
         return { ...prev, description: e.target.value };
       });
   };
 
   const handleSubmit = async () => {
-    if (!isValidFormData) alert("Invalid/Missing Input Data");
+    if (!isValidFormData) alert('Invalid/Missing Input Data');
 
     try {
       let data: APIError | UserDegree = await createDegree(degreeDataType);
 
-      if ("error" in data) {
+      if ('error' in data) {
         alert(data?.message);
         return;
       }
 
-      router.push("/creator/degree");
+      router.push('/creator/degree');
     } catch (error) {}
   };
 
   return (
     <>
-      <div
-        className={`${style["creator-task-details-form"]} common-panel-wrapper`}
-      >
+      <div className={`${style['creator-task-details-form']} `}>
         {/* Page Title */}
-        <Row className="p-15">
+        <Row style={{ padding: '15px 0' }}>
           <Col span={24}>
             <BreadCrumbNav item={breadCrumbItems} />
-            <PageTitle title="New Degree" />
+            <PageTitle title='New Degree' />
           </Col>
         </Row>
-        <Row gutter={[12, 0]} className="p-r-b-l-15 ">
+        <Row gutter={[12, 0]}>
           <Col span={12}>
-            <div className="border-box p-15">
+            <div className='border-box p-15'>
               <ImageUpload handleUpload={handleUpload} />
               <FormInput
-                label="Certification Title"
+                label='Certification Title'
                 onChange={handleOnChange}
-                placeholder="Add Title"
-                name="title"
+                placeholder='Add Title'
+                name='title'
               />
               <FormInput
-                label="Add Link"
-                type="link"
+                label='Add Link'
+                type='link'
                 onChange={handleOnChange}
-                placeholder="Add Certification Link"
-                name="degreeLink"
+                placeholder='Add Certification Link'
+                name='degreeLink'
               />
-              <div className="form-group">
-                <label htmlFor="requirement">Requirement</label>
+              <div className='form-group'>
+                <label htmlFor='requirement'>Requirement</label>
                 <FormTextArea
-                  placeholder="Description"
+                  placeholder='Description'
                   onChange={handleOnChange}
-                  name="description"
+                  name='description'
                 />
               </div>
-              <Flex gap={"middle"} justify="end">
+              <Flex gap={'middle'} justify='end'>
                 <PrimaryButton
-                  text="Cancel"
-                  variant="secondary"
+                  text='Cancel'
+                  variant='secondary'
                   onClick={() => router.back()}
                 />
                 <PrimaryButton
                   disabled={!isValidFormData(degreeDataType)}
-                  text="Save"
-                  variant="primary"
+                  text='Save'
+                  variant='primary'
                   onClick={handleSubmit}
                 />
               </Flex>
