768ca9db0703e38ce4aaaee4fdae3993b4cb8850
Prasun Roy <prasun.roy@fleapo.com>
2024-01-06 09:37:04 +0530

    Fixed Build Fail Error

--- components/common/texteditor.tsx
+++ components/common/texteditor.tsx
@@ -1,15 +1,15 @@
-import JoditEditor from 'jodit-react';
-import { useRef, useState } from 'react';
+import JoditEditor from "jodit-react";
+import { useRef, useState } from "react";
 
 const TextEditor = () => {
   const editor = useRef(null);
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
 
   const config2 = {
     useSearch: false,
     spellcheck: false,
-    enter: 'P',
-    defaultMode: '1',
+    enter: "p" as const,
+    defaultMode: 1,
     toolbarAdaptive: false,
     toolbarSticky: false,
     showCharsCounter: false,
@@ -21,42 +21,42 @@ const TextEditor = () => {
 
     // buttons:
     //   'paragraph,bold,strikethrough,underline,italic,|,superscript,subscript,|,ul,ol,|,|,font,fontsize,brush,,link,|,align,undo,redo',
-    buttons: 'bold,underline,italic,|,ul,ol',
-    editorCssClass: 'alic',
-    placeHolder: '',
+    buttons: "bold,underline,italic,|,ul,ol",
+    editorCssClass: "alic",
+    placeHolder: "",
     controls: {
       fontsize: {
         list: [
-          '8',
-          '9',
-          '10',
-          '11',
-          '12',
-          '14',
-          '16',
-          '18',
-          '24',
-          '30',
-          '36',
-          '48',
-          '60',
-          '72',
-          '96',
-          '100',
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "14",
+          "16",
+          "18",
+          "24",
+          "30",
+          "36",
+          "48",
+          "60",
+          "72",
+          "96",
+          "100",
         ],
       },
       font: {
-        command: 'fontname',
+        command: "fontname",
         list: {
-          '': 'Default',
-          "'Open Sans',sans-serif": 'Open Sans',
-          'Helvetica,sans-serif': 'Helvetica',
-          'Arial,Helvetica,sans-serif': 'Arial',
-          'Georgia,serif': 'Georgia',
-          'Impact,Charcoal,sans-serif': 'Impact',
-          'Tahoma,Geneva,sans-serif': 'Tahoma',
-          "'Times New Roman',Times,serif": 'Times New Roman',
-          'Verdana,Geneva,sans-serif': 'Verdana',
+          "": "Default",
+          "'Open Sans',sans-serif": "Open Sans",
+          "Helvetica,sans-serif": "Helvetica",
+          "Arial,Helvetica,sans-serif": "Arial",
+          "Georgia,serif": "Georgia",
+          "Impact,Charcoal,sans-serif": "Impact",
+          "Tahoma,Geneva,sans-serif": "Tahoma",
+          "'Times New Roman',Times,serif": "Times New Roman",
+          "Verdana,Geneva,sans-serif": "Verdana",
         },
       },
     },
@@ -67,9 +67,9 @@ const TextEditor = () => {
       ref={editor}
       value={content}
       config={config2}
-      tabIndex={1}
+      // tabIndex={1}  -- Commented as it doesn't exist and build failed
       onBlur={(newContent) => setContent(newContent)}
-      buttons={false}
+      // buttons={false}  -- Commented as it doesn't exist and build failed
       onChange={(newContent) => {}}
     />
   );
