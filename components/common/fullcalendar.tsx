c5322ba963d02316c6b3d842987322b4d424ac29
Prasun Roy <prasun.roy@fleapo.com>
2024-01-09 17:51:50 +0530

    Integrated Creator Get Habit Data API for Calender

--- components/common/fullcalendar.tsx
+++ components/common/fullcalendar.tsx
@@ -1,58 +1,73 @@
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-import interactionPlugin from '@fullcalendar/interaction'; // import the interaction plugin
-import FullCalendar from '@fullcalendar/react';
-import { Progress } from 'antd';
-import React from 'react';
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // import the interaction plugin
+import FullCalendar from "@fullcalendar/react";
+import React from "react";
 
-const customEvents = [
-  {
-    title: 'Tagmango Event',
-    start: '2024-01-15',
-    end: '2024-01-15',
-    progress: <Progress percent={50} />,
-    backgroundColor: '#fa6800',
-    borderColor: '#fa6800',
-  },
-  {
-    title: 'Fleapo Event 1',
-    start: '2024-01-15',
-    end: '2024-01-15',
-    progress: <Progress percent={20} />,
-    backgroundColor: '#d80473',
-    borderColor: '#d80473',
-  },
-  {
-    title: 'Fleapo Event 3',
-    start: '2024-01-03',
-    end: '2024-01-03',
-    progress: <Progress percent={20} />,
-    backgroundColor: '#d80473',
-    borderColor: '#d80473',
-  },
-];
+interface HabitCalenderType {
+  title: string;
+  start: string | Date;
+  end: string | Date;
+  progress: React.ReactNode;
+  backgroundColor: string;
+  borderColor: string;
+}
+
+interface FullCalendarDataType {
+  data: HabitCalenderType[];
+}
+
+// const customEvents = [
+//   {
+//     title: "Tagmango Event",
+//     start: "2024-01-15",
+//     end: "2024-01-15",
+//     progress: <Progress percent={50} />,
+//     backgroundColor: "#fa6800",
+//     borderColor: "#fa6800",
+//   },
+//   {
+//     title: "Fleapo Event 1",
+//     start: "2024-01-15",
+//     end: "2024-01-15",
+//     progress: <Progress percent={20} />,
+//     backgroundColor: "#d80473",
+//     borderColor: "#d80473",
+//   },
+//   {
+//     title: "Fleapo Event 3",
+//     start: "2024-01-03",
+//     end: "2024-01-05",
+//     progress: <Progress percent={20} />,
+//     backgroundColor: "#d80473",
+//     borderColor: "#d80473",
+//   },
+// ];
 
 const renderEventContent = (eventInfo: any) => {
   return (
-    <div className='on-calendar-content'>
+    <div className="on-calendar-content">
       <h6>{eventInfo.event.title}</h6>
       <div>{eventInfo.event.extendedProps.progress}</div>
     </div>
   );
 };
 
-export default class FullCalendarData extends React.Component {
+export default class FullCalendarData extends React.Component<FullCalendarDataType> {
   render() {
+    const { data } = this.props;
+
     return (
-      <div className='full-calendar'>
+      <div className="full-calendar">
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin]}
-          initialView='dayGridMonth'
+          initialView="dayGridMonth"
           editable={true}
           droppable={true}
           weekends={false}
-          height='auto'
-          events={customEvents}
-          eventTextColor='#fff'
+          height="auto"
+          eventClick={(arg) => console.log(arg)}
+          events={data}
+          eventTextColor="#fff"
           eventContent={renderEventContent}
         />
       </div>
--- src/app/creator/productivity/habits/page.tsx
+++ src/app/creator/productivity/habits/page.tsx
@@ -1,44 +1,89 @@
-'use client';
-import { Col, Row } from 'antd';
-import { useRouter } from 'next/navigation';
-import { PrimaryButton } from '../../../../../components/common/button';
-import FullCalendarData from '../../../../../components/common/fullcalendar';
-import { AddIcon } from '../../../../../components/common/icons';
-import PageTitle from '../../../../../components/pagetitle';
-import style from '../../../../../style/creator.module.scss';
+"use client";
+import useAPI from "@/hooks/useApi";
+import { CreateCreatorHabitType } from "@/types";
+import { Col, Progress, Row } from "antd";
+import dayjs from "dayjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { PrimaryButton } from "../../../../../components/common/button";
+import FullCalendarData from "../../../../../components/common/fullcalendar";
+import { AddIcon } from "../../../../../components/common/icons";
+import PageTitle from "../../../../../components/pagetitle";
+import style from "../../../../../style/creator.module.scss";
+
+interface HabitCalenderType {
+  title: string;
+  start: string | Date;
+  end: string | Date;
+  progress: React.ReactNode;
+  backgroundColor: string;
+  borderColor: string;
+}
 
 const CreatorHabit = () => {
   const router = useRouter();
+  const { getCreatorHabits } = useAPI();
+  const [habitData, setHabitData] = useState<HabitCalenderType[]>([]);
+
+  useEffect(() => {
+    fetchHabits();
+  }, []);
+
+  const fetchHabits = async () => {
+    try {
+      let data = await getCreatorHabits();
+      if (data && Array.isArray(data)) setHabitData(transformHabitData(data));
+    } catch (error) {}
+  };
+
+  const transformHabitData = (
+    data: CreateCreatorHabitType[]
+  ): HabitCalenderType[] => {
+    return data.map((item, index) => ({
+      title: item.title,
+      start: dayjs(item.date!).format("YYYY-MM-DD"),
+      end: dayjs(item.date!).format("YYYY-MM-DD")!,
+      progress: (
+        <Progress
+          percent={parseFloat(
+            ((item.completedCount! / item.subscriberCount!) * 100).toFixed(2)
+          )}
+        />
+      ),
+      backgroundColor: index % 2 ? "#fa6800" : "d80473",
+      borderColor: index % 2 ? "#fa6800" : "d80473",
+    }));
+  };
 
   const handleButtonClick = () => {
-    router.push('/creator/productivity/habits/addhabits');
+    router.push("/creator/productivity/habits/addhabits");
   };
 
   return (
     <>
-      <div className={`${style['event-tasks-page']} common-panel-wrapper`}>
+      <div className={`${style["event-tasks-page"]} common-panel-wrapper`}>
         {/* Page Title */}
         <Row
-          justify={'space-between'}
-          style={{ alignItems: 'center' }}
-          className='p-15'
+          justify={"space-between"}
+          style={{ alignItems: "center" }}
+          className="p-15"
         >
           <Col span={12}>
-            <PageTitle title='Habits Tracker' />
+            <PageTitle title="Habits Tracker" />
           </Col>
-          <Col span={12} style={{ display: 'flex', justifyContent: 'end' }}>
+          <Col span={12} style={{ display: "flex", justifyContent: "end" }}>
             <PrimaryButton
-              text='Add Habits'
+              text="Add Habits"
               icon={<AddIcon />}
-              variant='primary'
+              variant="primary"
               onClick={handleButtonClick}
             />
           </Col>
         </Row>
         <Row>
           <Col span={24}>
-            <div className='p-15'>
-              <FullCalendarData />
+            <div className="p-15">
+              <FullCalendarData data={habitData} />
             </div>
           </Col>
         </Row>
